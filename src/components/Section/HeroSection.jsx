f6c86a4852d4d9e37ddb7bdddb185e1ab6e4919f
Iqbal Adrian <iqbaladriam@gmail.com>
2025-10-22 14:42:09 +0700

    Fixing Bug

--- src/components/Section/HeroSection.jsx
+++ src/components/Section/HeroSection.jsx
@@ -1,24 +1,31 @@
-<section className="min-h-[10vh] md:min-h-[10vh] w-full flex flex-col items-center justify-start bg-white px-4 py-4">
-  {/* Logo */}
-  <img
-    src="/Assets/logo-dkm group/logodkm.png"
-    alt="Dwipa Kharisma Mitra"
-    className="w-32 mb-2"
-  />
+export function HeroSection() {
+  return (
+    <>
+      <section className="min-h-[10vh] md:min-h-[10vh] w-full flex flex-col items-center justify-start bg-white px-4 py-4">
+        {/* Logo */}
+        <img
+          src="/Assets/logo-dkm group/logodkm.png"
+          alt="Dwipa Kharisma Mitra"
+          className="w-32 mb-2"
+        />
 
-  {/* Title */}
-  <h2 className="text-[#DA1D2C] text-center font-semibold text-base leading-tight">
-    Trusted Container Depot & <br />
-    Logistics Solutions in Indonesia
-  </h2>
+        {/* Title */}
+        <h2 className="text-[#DA1D2C] text-center font-semibold text-base leading-tight">
+          Trusted Container Depot & <br />
+          Logistics Solutions in Indonesia
+        </h2>
 
-  {/* Buttons */}
-  <div className="flex gap-3 mt-2">
-    <button className="bg-gray-100 font-medium px-4 py-2 rounded-md shadow-sm">
-      Official Website
-    </button>
-    <button className="bg-gray-100 font-medium px-4 py-2 rounded-md shadow-sm">
-      Company Profile
-    </button>
-  </div>
-</section>;
+        {/* Buttons */}
+        <div className="flex gap-3 mt-2">
+          <button className="bg-gray-100 font-medium px-4 py-2 rounded-md shadow-sm">
+            Official Website
+          </button>
+          <button className="bg-gray-100 font-medium px-4 py-2 rounded-md shadow-sm">
+            Company Profile
+          </button>
+        </div>
+      </section>
+      ;
+    </>
+  );
+}
